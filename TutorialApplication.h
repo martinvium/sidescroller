f3ed1bd191940f196677d9ca900fa818a8c47093
Martin Vium <martin.vium@gmail.com>
2011-04-21 16:32:36 +0200

    Walking robot

--- TutorialApplication.h
+++ TutorialApplication.h
@@ -18,8 +18,7 @@ This source file is part of the
 #define __TutorialApplication_h_
 
 #include "BaseApplication.h"
-#include <Terrain/OgreTerrain.h>
-#include <Terrain/OgreTerrainGroup.h>
+#include <deque>
 
 class TutorialApplication : public BaseApplication
 {
@@ -30,23 +29,20 @@ public:
 protected:
     virtual void createScene(void);
     virtual void createFrameListener(void);
+    virtual bool nextLocation(void);
+    virtual bool frameRenderingQueued(const Ogre::FrameEvent &evt);
 
-    // Ogre::FrameListener
-    virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt );
-    // OIS::KeyListener
-    virtual bool keyPressed( const OIS::KeyEvent& evt );
-    virtual bool keyReleased( const OIS::KeyEvent& evt );
-    // OIS::MouseListener
-    virtual bool mouseMoved( const OIS::MouseEvent& evt );
-    virtual bool mousePressed( const OIS::MouseEvent& evt, OIS::MouseButtonID id );
-    virtual bool mouseReleased( const OIS::MouseEvent& evt, OIS::MouseButtonID id );
+    Ogre::Real mDistance;                  // The distance the object has left to travel
+    Ogre::Vector3 mDirection;              // The direction the object is moving
+    Ogre::Vector3 mDestination;            // The destination the object is moving towards
 
-    Ogre::Real mRotate;          // The rotate constant
-    Ogre::Real mMove;            // The movement constant
+    Ogre::AnimationState *mAnimationState; // The current animation state of the object
 
-    Ogre::SceneNode *mCamNode;   // The SceneNode the camera is currently attached to
+    Ogre::Entity *mEntity;                 // The Entity we are animating
+    Ogre::SceneNode *mNode;                // The SceneNode that the Entity is attached to
+    std::deque<Ogre::Vector3> mWalkList;   // The list of points we are walking to
 
-    Ogre::Vector3 mDirection;     // Value to move in the correct direction
+    Ogre::Real mWalkSpeed;                 // The speed at which the object is moving
 };
 
 #endif // #ifndef __TutorialApplication_h_
